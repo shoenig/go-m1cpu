bd5f028d6d60ea65d226d84aba3e52634afea455
联盟少侠 <admin@shaoxia.xyz>
2023-11-09 12:18:12 +0800

    fix error: 'kIOMasterPortDefault' is unavailable: not available on iOS
    test on m1 cpu ,macos and ios build

--- cpu.go
+++ cpu.go
@@ -67,7 +67,7 @@ package m1cpu
 //
 //   CFMutableDictionaryRef matching = IOServiceMatching("AppleARMIODevice");
 //   io_iterator_t  iter;
-//   IOServiceGetMatchingServices(kIOMainPortDefault, matching, &iter);
+//   IOServiceGetMatchingServices(MACH_PORT_NULL, matching, &iter);
 //
 //   const size_t bufsize = 512;
 //   io_object_t obj;
